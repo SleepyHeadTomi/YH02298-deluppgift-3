c596c169c19ec95ed5e14bf6a3f8046f0bbe3128
Tomislav Vuckovic <vuckovic.tomislav1@gmail.com>
2025-11-29 14:32:53 +0100

    test: refactor tests and add conftest.py

---
+++ integrtion_tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+import requests
+
+BASE_URL = "https://fakestoreapi.com"
+
+@pytest.fixture
+def expected_data():
+
+    expected_data = {
+        "id": 1,
+        "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        "price": 109.95,
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+        "rating": {"rate": 3.9, "count": 120}
+    }
+
+    return expected_data
+
+@pytest.fixture
+def api_response():
+
+    response = requests.get(f"{BASE_URL}/products")
+
+    return response
+
+@pytest.fixture
+def api_data_item_1(expected_data):
+
+    response = requests.get(f"{BASE_URL}/products/{expected_data.get("id")}")
+    data = response.json()
+
+    return data
--- integrtion_tests/test_fake_store_api.py
+++ integrtion_tests/test_fake_store_api.py
@@ -1,36 +1,19 @@
-import pytest
-import requests
+def test_get_products_returns_statuscode_200(api_response):
 
-BASE_URL = "https://fakestoreapi.com"
-total_products = 20
-expected_data = {
-        "id": 1,
-        "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-        "price": 109.95,
-        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-        "category": "men's clothing",
-        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
-        "rating": {"rate": 3.9, "count": 120}
-    }
+    assert api_response.status_code == 200
 
-def test_get_products_returns_statuscode_200():
-    response = requests.get(f"{BASE_URL}/products")
 
-    assert response.status_code == 200
+def test_get_products_returns_20_items(api_response):
+    total_products = 20
 
-def test_get_products_returns_20_items():
-    response = requests.get(f"{BASE_URL}/products")
-
-    data = response.json()
+    data = api_response.json()
 
     assert len(data) == total_products
 
-def test_get_id_1_returns_correct_data():
-    response = requests.get(f"{BASE_URL}/products/{expected_data.get("id")}")
 
-    data = response.json()
+def test_get_item_1_returns_correct_data(api_data_item_1, expected_data):
 
-    assert len(data) == len(expected_data)
+    assert len(api_data_item_1) == len(expected_data)
 
     for key, expected_value in expected_data.items():
-        assert data.get(key) == expected_value
+        assert api_data_item_1.get(key) == expected_value
