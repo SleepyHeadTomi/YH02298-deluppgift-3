a6534f7b782b782e843633e6e62f5898584547c7
Tomislav Vuckovic <vuckovic.tomislav1@gmail.com>
2026-01-30 16:37:53 +0100

    test: remove conftest and update tests

--- integrtion_tests/test_fake_store_api.py
+++ integrtion_tests/test_fake_store_api.py
@@ -1,19 +1,36 @@
-def test_get_products_returns_statuscode_200(api_response):
+import requests
 
-    assert api_response.status_code == 200
+BASE_URL = "https://fakestoreapi.com"
 
-
-def test_get_products_returns_20_items(api_response):
-    total_products = 20
-
-    data = api_response.json()
-
-    assert len(data) == total_products
+def test_get_products_returns_statuscode_200():
+    response = requests.get(f"{BASE_URL}/products")
+    assert response.status_code == 200
 
 
-def test_get_item_1_returns_correct_data(api_data_item_1, expected_data):
+def test_get_products_returns_20_items():
+    response = requests.get(f"{BASE_URL}/products")
 
-    assert len(api_data_item_1) == len(expected_data)
+    data = response.json()
 
-    for key, expected_value in expected_data.items():
-        assert api_data_item_1.get(key) == expected_value
+    assert len(data) == 20
+
+def test_get_item_1_returns_correct_fields():
+    response = requests.get(f"{BASE_URL}/products/1")
+
+    product_data = response.json()
+    assert product_data["id"] == 1
+
+    assert "title" in product_data
+    assert "price" in product_data
+    assert "description" in product_data
+
+
+def test_get_item_1_returns_correct_data():
+    response = requests.get(f"{BASE_URL}/products/1")
+
+    product_data = response.json()
+
+    assert product_data["id"] == 1
+    assert product_data["title"] == "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
+    assert product_data["price"] == 109.95
+    assert product_data["description"] == "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
